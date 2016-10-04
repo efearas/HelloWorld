2bb9a03fdb5a283a68a891f1c519920d15a7bd4b
efe <efe@efe-PC>
2016-10-04 15:28:38 +0300

    c1

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,13 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i = 0;i<4;i++)
+		{
+			System.out.println("Helllooo");
+			System.out.println("Helllooo2");
+		}
+	}
+
+}
